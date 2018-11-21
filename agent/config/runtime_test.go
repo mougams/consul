92986ee95a459b4d9ce792ff6034702add8ec3c5
Thibault Gilles <t.gilles@criteo.com>
2018-11-21 15:20:57 +0100

    Add an option to configure watchLimit in state_store.go
    watchLimit controls how many watches are allowed for each blocking
    queries. This adds a configuration option named watch_soft_limit to
    tweak this value.

--- agent/config/runtime_test.go
+++ agent/config/runtime_test.go
@@ -18,6 +18,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/hashicorp/consul/agent/consul"
 	"github.com/hashicorp/consul/agent/structs"
 	"github.com/hashicorp/consul/lib"
 	"github.com/hashicorp/consul/testutil"
@@ -3429,7 +3430,8 @@ func TestFullConfig(t *testing.T) {
 					"key": "sl3Dffu7",
 					"args": ["dltjDJ2a", "flEa7C2d"]
 				}
-			]
+			],
+			"watch_soft_limit": ` + fmt.Sprint(consul.DefaultSoftWatchLimit) + `
 		}`,
 		"hcl": `
 			acl_agent_master_token = "furuQD0b"
@@ -3980,7 +3982,8 @@ func TestFullConfig(t *testing.T) {
 				datacenter = "fYrl3F5d"
 				key = "sl3Dffu7"
 				args = ["dltjDJ2a", "flEa7C2d"]
-			}]
+			}],
+			watch_soft_limit = ` + fmt.Sprint(consul.DefaultSoftWatchLimit) + `
 		`}
 
 	tail := map[string][]Source{
@@ -4628,6 +4631,7 @@ func TestFullConfig(t *testing.T) {
 				"args":       []interface{}{"dltjDJ2a", "flEa7C2d"},
 			},
 		},
+		WatchSoftLimit: consul.DefaultSoftWatchLimit,
 	}
 
 	warns := []string{
@@ -5215,6 +5219,7 @@ func TestSanitize(t *testing.T) {
 		"VersionPrerelease": "",
 		"Watches": [],
 		"AllowWriteHTTPFrom": []
+		"WatchSoftLimit": 0
 	}`
 	b, err := json.MarshalIndent(rt.Sanitized(), "", "    ")
 	if err != nil {
