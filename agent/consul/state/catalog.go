9b0f7b83aefd88d8f79558b4f492f6f73f13eddf
Thibault Gilles <t.gilles@criteo.com>
2018-11-30 15:22:15 +0100

    Share blocking queries between RPC requests
    Only run one blocking query per request and share the result with all
    RPC calls. This optimizes the blocking query process when many agents
    are watching the same service.

--- agent/consul/state/catalog.go
+++ agent/consul/state/catalog.go
@@ -1965,7 +1965,7 @@ func (s *Store) CheckServiceTagNodes(ws memdb.WatchSet, serviceName string, tags
 		svc := service.(*structs.ServiceNode)
 		serviceExists = true
 		if !serviceTagsFilter(svc, tags) {
-			results = append(results, svc)
+			results = append(results, service.(*structs.ServiceNode))
 		}
 	}
 
